3ec51f2345766f3bd12a7456d0ca2de7e3687706
Ebube Okoli <ebube@Ebubes-MacBook-Pro.local>
2022-06-23 16:57:25 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let fullName = document.getElementById("name");
+let country = document.getElementById("country");
+let height = document.getElementById("height");
+
+setTimeout(() => {
+    country.innerHTML = 'Nigeria';
+    fullName.innerHTML = 'Ebube Okoli';
+    height.innerHTML = '185cm';
+}, 2000);
